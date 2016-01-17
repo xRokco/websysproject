7b09552c3b0a704dc6c80012c2d65ec087e24770
rokco <mattcarrick13@gmail.com>
2016-01-17 16:37:36 +0000

    Changed the user dropdown button to be more material-y. Tried to fix the logged in bug on the landing page but couldn't

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,11 @@
 
 @section('content')
 <div class="container">
-      <br><br>
       <h4 class="header center orange-text">Hi {{ Auth::user()->name }}, you are now logged in.</h4>
       <p class="light center">Please check out the driving directions from {{ Auth::user()->direction }} to the RDS, Dublin below.</p>
 </div>
-
-
 <div class="container">
-      <h2 class="header center orange-text"><iframe width="800" height="600" frameborder="0" style="border:0"
+      <h2 class="header center orange-text"><iframe width="700" height="525" frameborder="0" style="border:0"
 src="https://www.google.com/maps/embed/v1/directions?origin={{ Auth::user()->direction }}&destination=place_id:ChIJR4pGA8YOZ0gRT_oe5cS4-t0&key=AIzaSyBFaySDzqmlKyFwdG9qGWxGD3rjM1Ub0Bg" allowfullscreen></iframe></h2>
       <br><br>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,55 +9,64 @@
 
     {!! MaterializeCSS::include_full() !!}
 
-    <style>
-        .fa-btn {
-            margin-right: 6px;
-        }
-    </style>
 </head>
 <body id="app-layout" class="amber lighten-5">
     <nav class="green" role="navigation">
         <div class="nav-wrapper container"><a id="logo-container" href="/" class="brand-logo">WebSysProject</a>
-            <ul class="right hide-on-med-and-down">
+            <ul class="right">
                 @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
                         <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
+                            <!-- Dropdown Trigger -->
+                            <a style="margin:0px" class='dropdown-button btn transparent' href='#' data-beloworigin="true" data-hover='false' data-activates='dropdown1'>{{ Auth::user()->name }}</a>
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            <ul id='dropdown1' class='dropdown-content'>
+                                <li><a href="{{ url('/home') }}">Home</a></li>
+                                <li><a href="{{ url('/logout') }}">Logout</a></li>
                             </ul>
-                        </li>
                     @endif
             </ul>
-
-            <ul id="nav-mobile" class="side-nav">
-                @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-            </ul>
-            <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
         </div>
     </nav>
-
+    
     @yield('content')
 
-    
+    <footer class="page-footer green">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">{{ Auth::user() }}</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Settings</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+  </footer>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
