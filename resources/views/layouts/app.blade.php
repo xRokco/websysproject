074cf1b50d11229970bd5e1332792647cdc0b8dd
rokco <mattcarrick13@gmail.com>
2016-01-20 19:05:26 +0000

    some changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,17 +14,19 @@
     <nav class="teal" role="navigation">
         <div class="nav-wrapper container"><a id="logo-container" href="/" class="brand-logo">WebSysProject</a>
             <ul class="right">
-                @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                            <!-- Dropdown Trigger -->
+                @if (Auth::check())
+                        
+                        <!-- Dropdown Trigger -->
                             <a style="margin:0px" class='dropdown-button btn transparent' href='#' data-beloworigin="true" data-hover='false' data-activates='dropdown1'>{{ Auth::user()->name }}</a>
 
                             <ul id='dropdown1' class='dropdown-content'>
-                                <li><a href="{{ url('/home') }}">Home</a></li>
+                                <li><a href="{{ url('/dash') }}">Home</a></li>
                                 <li><a href="{{ url('/logout') }}">Logout</a></li>
                             </ul>
+                    @else
+                            <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+
                     @endif
             </ul>
         </div>
@@ -37,14 +39,9 @@
             <div class="container">
             Made by <a class="grey-text text-lighten-4" href="http://materializecss.com">Materialize</a>
             <a class="grey-text text-lighten-4 right" href="#!">© Web Systems Group 2016</a>
+            {{ Auth::guest() }}
             </div>
           </div>
     </footer>
-
-    <!--  Scripts-->
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="js/materialize.js"></script>
-  <script src="js/init.js"></script>
-
   </body>
 </html>
