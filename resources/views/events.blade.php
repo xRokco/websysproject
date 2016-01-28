39bb3c08bf05411869459b816a2a3c6453da46c3
rokco <mattcarrick13@gmail.com>
2016-01-28 13:56:09 +0000

    Removed page navigation

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -13,7 +13,7 @@
                 <div class="row grey lighten-5 valign-wrapper" id="event">
                     <!-- Event Image -->
                     <div class="col center s3">
-                        <img class="responsive-img" src='img/event_images/{{ $event->image }}' />
+                        <img class="responsive-img circle" src='img/event_images/{{ $event->image }}' style='max-height:200px' />
                     </div>
                     
                     <!-- Event Description -->
@@ -34,26 +34,6 @@
             @endforeach
 
             <br>
-            
-            <!-- Page Numbers -->
-            <div class="container">
-                <ul class="pagination center">
-                    <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-                    <li class="active"><a href="#!">1</a></li>
-                    <li class="waves-effect"><a href="#!">2</a></li>
-                    <li class="waves-effect"><a href="#!">3</a></li>
-                    <li class="waves-effect"><a href="#!">4</a></li>
-                    <li class="waves-effect"><a href="#!">5</a></li>
-                    <li class="waves-effect"><a href="#!"><i class="material-icons teal-text">chevron_right</i></a></li>
-                </ul>
-            </div>
         </div>
     </div>
-
-    <!-- Footer ( footer.html ) -->
-
-    <!--  Scripts-->
-    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="js/materialize.js"></script>
-    <script src="js/init.js"></script>
 @endsection
