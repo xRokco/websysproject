522b57a67465db017fae4c3d5ae27925b36b0dee
conmolloy <conmolloy@gmail.com>
2016-02-04 14:20:29 +0000

    Added the the photos and hopefully changed the header to the new one

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,53 +1,56 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<html lang="en">
+		<head>
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+			<title>Home Page</title>
+			{!! MaterializeCSS::include_full() !!}
+			
+			<style type="text/css">
+				body {
+					display: flex;
+					min-height: 100vh;
+					flex-direction: column;
+				}
 
-    <title>Laravel</title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    {!! MaterializeCSS::include_full() !!}
+				main {
+					flex: 1 0 auto;
+				}
+			</style>
 
-    <style type="text/css">
-        body {
-            display: flex;
-            min-height: 100vh;
-            flex-direction: column;
-        }
-
-        main {
-            flex: 1 0 auto;
-        }
-    </style>
-
-</head>
-<body id="app-layout" class="lighten-5">
-    <nav class="teal" role="navigation">
-        <div class="nav-wrapper container"><a id="logo-container" href="/" class="brand-logo">WebSysProject</a>
-            <ul class="right hide-on-med-and-down">
-                @if (Auth::check())     
-                    <!-- Dropdown Trigger -->
-                    <a style="margin:0px" class='dropdown-button btn transparent' href='#' data-beloworigin="true" data-hover='false' data-activates='dropdown1'>{{ Auth::user()->name }}</a>
-                    <ul id='dropdown1' class='dropdown-content'>
-                        <li><a href="{{ url('/dash') }}">Dash</a></li>
+			<!-- CSS  -->
+			<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+			<link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+			<link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+		</head>
+		<body>
+    
+			<nav class="red darken-2" role="navigation">
+				<div class="nav-wrapper container"><a id="logo-container" href="index.html" class="brand-logo">TRUMP</a>
+				<ul class="right hide-on-med-and-down">
+					@if (Auth::check())     
+						<!-- Dropdown Trigger -->
+						<ul id='dropdown1' class='dropdown-content'>
+							<li><a href="{{ url('/dash') }}">Dash</a></li>
+							<li><a href="{{ url('/events') }}">Events</a></li>
+							@if(Auth::user()->admin==1)
+								<li><a href="/admin">Admin</a></li>
+							@endif
+							<li><a href="{{ url('/logout') }}">Logout</a></li>
+						</ul>
+					@else
+						<li><a href="{{ url('/login') }}">Login</a></li>
 						<li><a href="{{ url('/events') }}">Events</a></li>
-                        @if(Auth::user()->admin==1)
-                            <li><a href="/admin">Admin</a></li>
-                        @endif
-                        <li><a href="{{ url('/logout') }}">Logout</a></li>
-                    </ul>
-                @else
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-					<li><a href="{{ url('/events') }}">Events</a></li>
-                    <li><a href="{{ url('/register') }}">Register</a></li>
-                @endif
+						<li><a href="{{ url('/register') }}">Register</a></li>
+					@endif
             </ul>
-        </div>
-    </nav>
-	
-	
-    <main>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+</nav>
+
+</body>
+</html>
+<main>
         @yield('content')
     </main>
 
