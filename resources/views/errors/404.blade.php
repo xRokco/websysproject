b5919c3539a2f1b0552ffa62e5716a5acc1f0666
rokco <mattcarrick13@gmail.com>
2016-02-06 19:20:35 +0000

    Some files didn't get added so here they are

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+    @section('content')
+
+    	<h1 class="center">Error 404: Page Not Found</h1>
+    	<h4 class="center">Why not <a href="/">return to out homepage?</a></h4>
+    @endsection
