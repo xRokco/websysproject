a0aee344725637fdc4bff7aff02484818222feac
rokco <mattcarrick13@gmail.com>
2016-02-06 23:56:49 +0000

    it now only shows 5 hotels, shows more useful info and looks a bit nicer.

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -76,58 +76,61 @@
                             src="https://www.google.com/maps/embed/v1/directions?origin={{ Auth::user()->direction }}&destination={{ $ev->venue }}, {{ $ev->city }}&key=AIzaSyBFaySDzqmlKyFwdG9qGWxGD3rjM1Ub0Bg" allowfullscreen></iframe>
                     </div>
 
-                    <table>
+                    <h5 class="center">Here's a couple of hotels within a couple of kilometers of the venue</h5>
+
+                    <table class="highlight">
                         <thead>
-                            <th data-field="id">Hotel Name</th>
-                            <th data-field="name">Website</th>
+                            <th data-field="id">Hotel</th>
+                            <th data-field="address">Address</th>
+                            <th data-field="phone">Phone Number</th>
                             <th data-field="rating">Rating</th>
                         </thead>
                         <tbody>
-                    <?php
-                      //$Address = urlencode($Address);
-                      $address_to_coordinates = "http://maps.googleapis.com/maps/api/geocode/xml?address=" . $ev->venue . ", " . $ev->city . "&sensor=true";
-                      $xml = simplexml_load_file($address_to_coordinates) or die("url not loading");
-                      $status = $xml->status;
-                      if ($status=="OK") {
-                          $Lat = $xml->result->geometry->location->lat;
-                          $Lon = $xml->result->geometry->location->lng;
-                          $LatLng = "$Lat,$Lon";
-                      }
+                            <?php
+                                $address_to_coordinates = "http://maps.googleapis.com/maps/api/geocode/xml?address=" . $ev->venue . ", " . $ev->city . "&sensor=true";
+                                $xml = simplexml_load_file($address_to_coordinates) or die("url not loading");
+                                $status = $xml->status;
 
-                      $place_id_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/xml?location=" . $LatLng . "&radius=1500&types=lodging&key=AIzaSyBFaySDzqmlKyFwdG9qGWxGD3rjM1Ub0Bg";
-                      $xml2 = simplexml_load_file($place_id_url) or die("url not loading");
-                      $status2 = $xml2->status;
-                      if($status2=="OK") {
-                        foreach ($xml2->result as $result) {
-                            $place_id = $result->place_id;
+                                if ($status=="OK") {
+                                    $Lat = $xml->result->geometry->location->lat;
+                                    $Lon = $xml->result->geometry->location->lng;
+                                    $LatLng = "$Lat,$Lon";
+                                }
 
-                            $place_details_url = "https://maps.googleapis.com/maps/api/place/details/xml?placeid=" . $place_id . "&key=AIzaSyBFaySDzqmlKyFwdG9qGWxGD3rjM1Ub0Bg";
-                            $xml3 = simplexml_load_file($place_details_url) or die("url not loading");
-                            $status3 = $xml3->status;
-                            if($status3=="OK"){
-                                $place_name = $xml3->result->name;
-                                $place_website = $xml3->result->website;
-                                $place_rating = $xml3->result->rating;
-                                echo "<tr>";
-                                echo "<td>" . $place_name . "</td>";
-                                echo "<td>" . $place_website . "</td>";
-                                echo "<td>" . $place_rating . "</td>";
-                                echo "</tr>";
-                            }
-                        }
+                                $place_id_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/xml?location=" . $LatLng . "&radius=1500&types=lodging&key=AIzaSyBFaySDzqmlKyFwdG9qGWxGD3rjM1Ub0Bg";
+                                $xml2 = simplexml_load_file($place_id_url) or die("url not loading");
+                                $status2 = $xml2->status;
 
-                      }
-                    ?>
-                    </tbody>
-            </table>
+                                if($status2=="OK") {
+                                    
+                                    for($i=0; $i<5; $i++) {
+                                        $place_id = $xml2->result[$i]->place_id;
 
-                    <a href="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={{ $LatLng }}&radius=1500&types=lodging&key=AIzaSyBFaySDzqmlKyFwdG9qGWxGD3rjM1Ub0Bg">Places</a>
+                                        $place_details_url = "https://maps.googleapis.com/maps/api/place/details/xml?placeid=" . $place_id . "&key=AIzaSyBFaySDzqmlKyFwdG9qGWxGD3rjM1Ub0Bg";
+                                        $xml3 = simplexml_load_file($place_details_url) or die("url not loading");
+                                        $status3 = $xml3->status;
 
+                                        if($status3=="OK"){
+                                            $place_name = $xml3->result->name;
+                                            $place_website = $xml3->result->website;
+                                            $place_rating = $xml3->result->rating;
+                                            $place_address = $xml3->result->formatted_address;
+                                            $place_phone = $xml3->result->formatted_phone_number;
+
+                                            echo "<tr>";
+                                            echo "<td><a href='" . $place_website . "'>" . $place_name . "</a></td>";
+                                            echo "<td>" . $place_address . "</td>";
+                                            echo "<td>" . $place_phone . "</td>";
+                                            echo "<td>" . $place_rating . "</td>";
+                                            echo "</tr>";
+                                        }
+                                    }
+                                }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
-            </div>
-
-            
-            
+            </div> 
         </div>
     </div>
 @endsection
