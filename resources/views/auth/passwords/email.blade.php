87bbf41cbb657c95e2741de25b2f178b9690e136
rokco <mattcarrick13@gmail.com>
2016-02-12 16:43:26 +0000

    changes password reset page button to be red

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -33,7 +33,7 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="red darken-3 btn btn-primary">
                                     <i class="fa fa-btn fa-envelope"></i>Send Password Reset Link
                                 </button>
                             </div>
