aea9d8c42a66cd498e7415e4e18beafa833122e4
rokco <mattcarrick13@gmail.com>
2016-02-22 17:46:49 +0000

    minor url changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
 		<body>
     
 			<nav class="red darken-3" role="navigation">
-				<div class="nav-wrapper container"><a id="logo-container" href="{{ url('/') }}" class="brand-logo"><img class="responsive-img" width="60px" src="/img/logo.png"><span style="margin-left:-10px">TRUMP</span></a>
+				<div class="nav-wrapper container"><a id="logo-container" href="{{ url('/') }}" class="brand-logo"><img class="responsive-img" width="60px" src="{{ url('/img/logo.png') }}"><span style="margin-left:-10px">TRUMP</span></a>
 					<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
 					<ul class="right hide-on-med-and-down">
 						@if (Auth::check())     
