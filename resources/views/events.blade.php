4df19e70cbef16bc2a2be72612334b2cd0e0add3
rokco <mattcarrick13@gmail.com>
2016-02-23 14:09:16 +0000

    better events page responsiveness

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -12,9 +12,9 @@
             <div class="divider"></div>
             <br>
             @foreach ($events as $event)
-                <div class="row grey lighten-5 valign-wrapper" id="event">
+                <div class="row grey lighten-5" id="event">
                     <!-- Event Image -->
-                    <div class="col center s3">
+                    <div class="col center m3 s12" style="margin-top:20px">
                         @if(Auth::check())
                             <a href="{{ url('/events/details') }}/{{ $event->id }}"><img class="responsive-img circle" src="{{ url('/img/event_images') }}/{{ $event->image }}" style="height:150px;width:150px;background-size:cover;" /></a>
                         @else
@@ -23,16 +23,16 @@
                     </div>
                     
                     <!-- Event Description -->
-                    <div class="left-align col s5">
+                    <div class="left-align col m5 s5 offset-s1">
                         <h5>{{ $event->name }}</h5></a>
                         <p>{{ $event->information }}</p>
                     </div>
                     
                     <!-- Event Details -->
-                    <div class="col s3" id="test">
-                        <p class="condensed light left-align valign-wrapper"><i class="material-icons">today</i>{{ $event->date }}</p>
-                        <p class="condensed light left-align valign-wrapper"><i class="material-icons">location_on</i>{{ $event->venue }}, {{ $event->city }}</p>
-                        <p class="condensed light left-align valign-wrapper"><i class="material-icons">payment</i>&euro;{{ $event->price }}</p>
+                    <div class="col m3 s5 offset-s1" id="test">
+                        <p class="condensed light left-align"><i class="material-icons">today</i>{{ $event->date }}</p>
+                        <p class="condensed light left-align"><i class="material-icons">location_on</i>{{ $event->venue }}, {{ $event->city }}</p>
+                        <p class="condensed light left-align"><i class="material-icons">payment</i>&euro;{{ $event->price }}</p>
                         @if(Auth::check())
                             <a class="btn red darken-3 lighten-1" href="{{ url('/events/details') }}/{{ $event->id }}">View Event</a>
                         @else
