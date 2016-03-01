1a6119f0444bb0ce432b0e10eba894fb3f9cc694
rokco <mattcarrick13@gmail.com>
2016-03-01 13:20:32 +0000

    more icon messing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
 			<!-- AddEvent -->
 			<script type="text/javascript" src="https://addevent.com/libs/atc/1.6.1/atc.min.js" async defer></script>
 			
-			<link rel="icon" href="{{ url('favicon.ico') }}" />
+			<link rel="icon" href="{{ url('favicon.png') }}" />
 		</head>
 		<body>
     		<!--Nav-->
